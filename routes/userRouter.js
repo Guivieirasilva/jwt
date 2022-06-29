b2b40c8931d8cbd53804b4f5f79006963f4d52c5
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-06-29 11:07:28 -0300

    configurando o controladores

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController')
 
-router.post('/register', (req,res) => {
-    console.log('Register');
-    res.send('Register');
-});
-router.post('/login', (req,res) => {
-    console.log('Login');
-    res.send('Login');
 
-});
+router.post('/register', userController.register );
+router.post('/login', userController.login);
 
 module.exports = router;
