ae29ceff11135baccd946cf4f9f9cc76848d331c
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-06-29 18:43:01 -0300

    configurando o login do usuario

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,9 +21,16 @@ const userController = {
             res.status(400).send(error)
         }
     },
-    login: (req,res) => {
-        console.log('login');
-        res.send('login');
+    login: async (req,res) => {
+
+        const selectedUser = await    
+    User.findOne({email:req.body.email})
+        if(!selectedUser) return res.status(400).send("Email or password incorrect")
+
+        const passwordAndUserMatch = bcrypt.compareSync(req.body.password, selectedUser.password)
+        if(!passwordAndUserMatch) return res.status(400).send("Email or password incorrect")
+
+        res.send("User Logged")
     },
 }
 
